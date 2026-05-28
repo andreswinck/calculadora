8bc5d43fc59de977e60ec27be9b50e6f4cf9a33b
Andres Winckler <95319893+andreswinck@users.noreply.github.com>
2026-05-28 15:42:53 -0300

    Remove version catalog from settings.gradle.kts
    Removed version catalog configuration from settings.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,15 +11,7 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
     }
-    versionCatalogs {
-        create("libs") {
-            from(files("gradle/libs.versions.toml"))
-        }
-    }
 }
 
 rootProject.name = "calculadora"
-
-// Forzamos a Gradle a encontrar la carpeta de la app en la raíz
 include(":app")
-project(":app").projectDir = file("app")
